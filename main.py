613f6a320ae3694ecb2b22041b6b747b39111969
ydc123 <892431401@qq.com>
2019-01-10 20:31:59 +0800

    acclerate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import random
+import time
 
 img = cv2.imread('test.png')
 mask = np.zeros(img.shape[:2], dtype=np.uint8)
@@ -19,20 +20,15 @@ def getdis(A, B, p, q):
     dis = abs(A[p[0]:p[0] + patch_len, p[1]:p[1] + patch_len] - B[q[0]:q[0] + patch_len, q[1]:q[1] + patch_len]).sum()
     return dis
 
-
-def fill(src, tar, fr, to):
-    tar[to[0]][to[1]] = src[fr[0]][fr[1]]
-
 def PatchMatch(img, mask, level, prev_match, max_level):
+    global start
     cor = []
     w, h = mask.shape
-    idx = np.zeros((w, h), dtype=np.int)
     tar = img * ((mask == 0)[:, :, None])
     match = np.zeros((w, h, 2), dtype=np.int)
     for i in range(w - patch_len):
         for j in range(h - patch_len):
             if mask[i][j] != 0:
-                idx[i][j] = len(cor)
                 cor.append([i, j])
                 if level == 0:
                     match[i][j] = np.array([np.random.randint(0, w - patch_len), np.random.randint(0, h - patch_len)])
@@ -49,9 +45,9 @@ def PatchMatch(img, mask, level, prev_match, max_level):
     for i in order:
         x, y = cor[i]
         value[i] = getdis(img, tar, match[x, y], cor[i])
-        fill(img, tar, match[x, y], cor[i])
-    for epoch in range(20):
-        print(epoch)
+        tar[x][y] = img[match[x][y][0]][match[x][y][1]]
+    cnt = np.zeros_like(mask)
+    for epoch in range(5):
         order = order[::-1]
         for i in order:
             x, y = cor[i]
@@ -61,25 +57,32 @@ def PatchMatch(img, mask, level, prev_match, max_level):
                     if newdis < value[i]:
                         value[i] = newdis
                         match[x, y] = match[x + movex[d], y + movey[d]]
-                        fill(img, tar, match[x, y], [x, y])
+                        tar[x][y] = img[match[x][y][0]][match[x][y][1]]
             R = max(w, h)
             v0 = match[x, y]
-            while R >= 1:
-                R /= 2.0
-                r = int(R)
-                u = v0 + np.random.randint(-r, r + 1, 2)
-                while u[0] < 0 or u[1] < 0 or u[0] + patch_len >= w or u[1] + patch_len >= h:
-                    u = v0 + np.random.randint(-r, r + 1, 2)
-                newdis = getdis(img, tar, u, cor[i])
-                if newdis < value[i]:
-                    value[i] = newdis
-                    match[x, y] = u
-                    fill(img, tar, match[x, y], [x, y])
+            R = max(w, h)
+            v0 = match[x, y].copy()
+            l_dx, r_dx = -v0[0], w - patch_len - v0[0]
+            l_dy, r_dy = -v0[1], h - patch_len - v0[1]
+            log2R = int(np.log2(R))
+            dx = np.random.randint(l_dx, r_dx, log2R)
+            dy = np.random.randint(l_dy, r_dy, log2R)
+            u = v0 + np.array([dx, dy]).T
+            newdis = np.zeros(log2R)
+            for j in range(log2R):
+                newdis[j] = getdis(img, tar, u[j], cor[i])
+            idx = np.argmin(newdis)
+            if newdis[idx] < value[i]:
+                value[i] = newdis[idx]
+                match[x, y] = u[idx]
+                # tar[x][y] = img[u[j][0]][u[j][1]]
     if level == max_level:
+        print('time: {}'.format(time.time() - start))
         tar = tar.astype(np.uint8)
         cv2.imshow("result.png", tar)
         cv2.waitKey()
     return match
+
 
 cv2.namedWindow('draw')
 cv2.setMouseCallback('draw', draw)
@@ -88,6 +91,9 @@ while True:
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
+cv2.imwrite('mask.png', mask)
+start = time.time()
+mask = cv2.imread('mask.png', 0)
 cv2.destroyWindow('draw')
 img = cv2.imread('test.png')
 imgs = [img.astype(np.int)]
@@ -100,5 +106,6 @@ while img.shape[0] > 10 * patch_len and img.shape[1] > 10 * patch_len:
 imgs = imgs[::-1]
 masks = masks[::-1]
 prev_match = None
+np.random.seed(1234)
 for i in range(len(imgs)):
     prev_match = PatchMatch(imgs[i], masks[i], i, prev_match, len(imgs) - 1)
